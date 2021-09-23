9aca8a92f66e69c1a4b8ef89ab047f70648faeac
Farzam Azhar <farzam.azher@square63.com>
2021-09-23 19:43:46 +0500

    Re: Heroku deploy

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    // how many messages should we buffer and send to AWS at once
+  batchSize: 50,
+
+  // at what port should our server be listening
+  serverPort: 3001,
+
+  // your cloudwatch log group
+  logGroup: 'WeGuideEngageCanaryLogGroup',
+
+  // prefix for all stream names (a random number will be appended)
+  logStreamPrefix: "dyno",
+
+  // we strongly recommend using env variables for the credentials, however you can still add them here
+  awsCredentials: {
+    region: "ap-southeast-2",
+  },
+
+  // access token, which will be sent from heroku on every request
+  accessToken: "sometoken"
+};
